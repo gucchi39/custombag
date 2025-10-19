ba09e510b4555863fc1f68682e3da351eaf4ea02
gucchi39 <gucchi39@users.noreply.github.com>
2025-10-19 18:09:26 +0900

    Remove .vite directory from Git tracking and update .gitignore

--- .vite/deps/react-dom.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-ANHCFZYD.js";
-import "./chunk-TTD4J7JF.js";
-import "./chunk-32E4H3EV.js";
-import "./chunk-G3PMV62Z.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
